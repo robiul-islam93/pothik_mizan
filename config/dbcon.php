6c5ba94afc6d88e8be2fe630a9a9ebf77cd9f4ff
Robiul Islam <robi.cnits@gmail.com>
2023-09-09 17:28:23 +0600

    config(dbcon file and function file) Setup

---
+++ config/dbcon.php
@@ -0,0 +1,30 @@
+<?php
+
+
+define('DB_SERVER',"localhost");
+define('DB_USERNAME',"root");
+define('DB_PASSWORD',"");
+define('DB_DATABASE',"pothik-mizan");
+
+$connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+if(!$connection){
+    die("Connection Failed:".mysqli_connect_error());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ config/function.php
@@ -0,0 +1,170 @@
+<?php
+
+session_start();
+
+// database connection start
+
+require 'dbcon.php';
+// database connection end
+
+
+
+// user security connection start
+function validate($inputDate){
+    global $connection;
+
+    $validateDate = mysqli_real_escape_string($connection , $inputDate);
+
+    return trim($validateDate);
+}
+
+// user security connection end
+
+
+// redirect url bar  function start
+
+function redirect($url, $status){
+    $_SESSION['status'] = "$status";
+    header('Location:'.$url);
+    exit(0);
+}
+
+// redirect url bar  function end
+
+
+// alerts massage function start
+
+function alertMessage(){
+    if(isset($_SESSION['status'])){
+        echo '<div class ="aleart rounded py-3 p-2 alert-success">
+        <h5>'.$_SESSION['status'].'</h5></div>';
+        unset($_SESSION['status']);
+    }
+}
+
+// alerts massage function end
+
+
+// getall data in database function start
+
+function getall($tableName){
+    global $connection;
+    $table = validate($tableName);
+
+    $query = "SELECT * FROM $table";
+    $result = mysqli_query($connection , $query);
+    return $result;
+}
+
+// getall data in database function end
+
+
+
+// user update function in php code start
+
+// get URL Id function start
+function paramnId($paramtype){
+    if(isset($_GET[$paramtype])){
+        if($_GET[$paramtype] != null){
+            return $_GET[$paramtype];
+        }
+        else{
+            echo "No Id Found";
+        }
+    }else{
+        echo "No Id Given";
+    }
+}
+
+// get URL Id function end
+
+// user update function in php code end
+
+
+//  database table get function start
+function getById($tablename, $id)
+{
+    global $connection;
+
+    $table = validate($tablename);
+    $id = validate($id);
+
+    $query = "SELECT * FROM $table WHERE id ='$id' LIMIT 1";
+    $result = mysqli_query($connection,$query);  
+
+    if($result){
+
+        if(mysqli_num_rows($result)==1){
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $response = [
+                'status' => 200,
+                'message' => 'Fected data',
+                'data' => $row
+            ];
+            return $response;
+        }
+        else
+        {
+            $response = [
+                'status' => 404,
+                'message' => 'No Data Found'
+            ];
+            return $response;
+        }
+
+    }else{
+        $response = [
+            'status' => 500,
+            'message' => 'Something Went Wrong'
+        ];
+        return $response;
+    }
+
+}
+
+//  database table get function end
+
+
+// user delete function start 
+
+function deletequery($tableName, $id){
+    global $connection;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $query = "DELETE FROM $table WHERE id = '$id' LIMIT 1";
+    $result = mysqli_query($connection, $query);
+    return $result;
+}
+
+// user delete function end
+
+
+// logout function start 
+
+function logoutSession(){
+    unset($_SESSION['auth']);
+    unset($_SESSION['LoggedInUserRole']);
+    unset($_SESSION['LoggedInUser']);
+}
+
+// logout function end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
